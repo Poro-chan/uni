5113aef0dba3b376c77631009c3d52c5512972b3
Noelle Kreidl <a12029871@unet.univie.ac.at>
2023-05-10 15:33:27 +0200

    Datipdp

---
+++ eddie-cim-translator/src/main/java/schemas/region/it/DatiPdp2GType.java
@@ -0,0 +1,73 @@
+package schemas.region.it;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "DatiPdp2GType", propOrder = {
+    "Trattamento",
+    "Tensione",
+    "Forfait",
+    "Ka",
+    "Kr",
+    "Kp"
+})
+
+public class DatiPdp2GType {
+    @XmlElement(name = "Trattamento")
+    protected String trattamento;
+    @XmlElement(name = "Tensione")
+    protected int tensione;
+    @XmlElement(name = "Forfait")
+    protected String forfait;
+    @XmlElement(name = "Ka")
+    protected String ka;
+    @XmlElement(name = "Kr")
+    protected String kr;
+    @XmlElement(name = "Kp")
+    protected String kp;
+
+    public String getTrattamento() {
+        return trattamento;
+    }
+    public void setTrattamento(String value) {
+        this.trattamento = value;
+    }
+
+    public int getTensione() {
+        return tensione;
+    }
+    public void setTensione(int value) {
+        this.tensione = value;
+    }
+
+    public String getForfait() {
+        return forfait;
+    }
+    public void setForfaito(String value) {
+        this.forfait = value;
+    }
+
+    public String getKa() {
+        return ka;
+    }
+    public void setKa(String value) {
+        this.ka = value;
+    }
+
+    public String getKr() {
+        return kr;
+    }
+    public void setKr(String value) {
+        this.kr = value;
+    }
+
+    public String getKp() {
+        return kp;
+    }
+    public void setKp(String value) {
+        this.kp = value;
+    }
+
+
+}
