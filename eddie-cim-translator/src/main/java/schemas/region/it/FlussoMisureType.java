a77c61838343fe60d6bc2b377b9edaf4a2901498
Maximilian Pertinger <a11829334@unet.univie.ac.at>
2023-05-10 16:13:55 +0200

    FlussoMisureType required fields added

--- eddie-cim-translator/src/main/java/schemas/region/it/FlussoMisureType.java
+++ eddie-cim-translator/src/main/java/schemas/region/it/FlussoMisureType.java
@@ -4,5 +4,30 @@ import javax.xml.bind.annotation.*;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "FlussoMisure")
+@XmlType(name = "FlussoMisureType", propOrder = {
+        "identificativiFlusso",
+        "datiPod"
+})
 public class FlussoMisureType {
+
+    @XmlElement(name = "IdentificativiFlusso", required = true)
+    protected IdentificativiFlussoType identificativiFlusso;
+    @XmlElement(name = "DatiPod", required = true)
+    protected DatiPod datiPod;
+
+    public IdentificativiFlussoType getIdentificativiFlusso() {
+        return identificativiFlusso;
+    }
+
+    public void setIdentificativiFlusso(IdentificativiFlussoType identificativiFlusso) {
+        this.identificativiFlusso = identificativiFlusso;
+    }
+
+    public DatiPod getDatiPod() {
+        return datiPod;
+    }
+
+    public void setDatiPod(DatiPod datiPod) {
+        this.datiPod = datiPod;
+    }
 }
