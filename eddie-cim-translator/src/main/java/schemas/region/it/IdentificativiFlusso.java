67a69505b543fffc9ac75e576cffdf358f1727a1
Noelle Kreidl <a12029871@unet.univie.ac.at>
2023-05-10 14:18:10 +0200

    IdentificativiFlusso

---
+++ eddie-cim-translator/src/main/java/schemas/region/it/IdentificativiFlusso.java
@@ -0,0 +1,47 @@
+package schemas.region.it;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "IdentificativiFlussoType", propOrder = {
+    "PIvaUtente",
+    "PIvaDistributore",
+    "CodContrDisp"
+})
+
+public class IdentificativiFlusso {
+
+    @XmlElement(name = "PIvaUtente")
+    protected int pivautente;
+    @XmlElement(name = "PIvaDistributore")
+    protected int pivadistributore;
+    @XmlElement(name = "CodContrDisp")
+    protected string codcontrdisp;
+
+    public int getPIvaUtente() {
+        return pivautente;
+    }
+    public void setPIvaUtente(int value) {
+        this.pivautente = value;
+    }
+
+    public int getPIvaDistributore() {
+        return pivadistributore;
+    }
+    public void setPIvaDistributore(int value) {
+        this.pivadistributore = value;
+    }
+
+    public int getCodContrDisp() {
+        return codcontrdisp;
+    }
+    public void setCodContrDisp(String value) {
+        this.codcontrdisp = value;
+    }
+}
