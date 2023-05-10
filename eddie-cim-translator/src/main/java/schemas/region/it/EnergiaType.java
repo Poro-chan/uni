5c5e64165b4bfa16e2588f5ed6f4d8d4fc7577ca
Noelle Kreidl <a12029871@unet.univie.ac.at>
2023-05-10 19:46:53 +0200

    Misura

---
+++ eddie-cim-translator/src/main/java/schemas/region/it/EnergiaType.java
@@ -0,0 +1,294 @@
+package schemas.region.it;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "EnergiaType", propOrder = {
+    "Dst",
+    "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10",
+    "E11", "E12", "E13", "E14","E15", "E16", "E17","E18", "E19",
+    "E20","E21","E22","E23","E24","E25","E26","E27","E28","E29",
+    "E30", "E31", "E32", "E33", "E34", "E35", "E36", "E37", "E38", "E39",
+    "E40", "E41", "E42", "E43", "E44", "E45", "E46", "E47", "E48", "E49",
+    "E50", "E51", "E52", "E53", "E54", "E55", "E56", "E57", "E58", "E59",
+    "E60", "E61", "E62", "E63", "E64", "E65", "E66", "E67", "E68", "E69",
+    "E70", "E71", "E72", "E73", "E74", "E75", "E76", "E77", "E78", "E79",
+    "E80","E81", "E82", "E83", "E84", "E85", "E86", "E87", "E88", "E89",
+    "E90", "E91", "E92", "E93", "E94", "E95", "E96"
+
+})
+
+public class DettaglioMisuraRFOv2Type {
+
+    @XmlElement(name = "Dst")
+    protected String dst;
+    @XmlElement(name = 'E1')
+    protected String e1;
+    @XmlElement(name = 'E2')
+    protected String e2;
+    @XmlElement(name = 'E3')
+    protected String e3;
+    @XmlElement(name = 'E4')
+    protected String e4;
+    @XmlElement(name = 'E5')
+    protected String e5;
+    @XmlElement(name = 'E6')
+    protected String e6;
+    @XmlElement(name = 'E7')
+    protected String e7;
+    @XmlElement(name = 'E8')
+    protected String e8;
+    @XmlElement(name = 'E9')
+    protected String e9;
+    @XmlElement(name = 'E10')
+    protected String e10;
+    @XmlElement(name = 'E11')
+    protected String e11;
+    @XmlElement(name = 'E12')
+    protected String e12;
+    @XmlElement(name = 'E13')
+    protected String e13;
+    @XmlElement(name = 'E14')
+    protected String e14;
+    @XmlElement(name = 'E15')
+    protected String e15;
+    @XmlElement(name = 'E16')
+    protected String e16;
+    @XmlElement(name = 'E17')
+    protected String e17;
+    @XmlElement(name = 'E18')
+    protected String e18;
+    @XmlElement(name = 'E19')
+    protected String e19;
+    @XmlElement(name = 'E20')
+    protected String e20;
+    @XmlElement(name = 'E21')
+    protected String e21;
+    @XmlElement(name = 'E22')
+    protected String e22;
+    @XmlElement(name = 'E23')
+    protected String e23;
+    @XmlElement(name = 'E24')
+    protected String e24;
+    @XmlElement(name = 'E25')
+    protected String e25;
+    @XmlElement(name = 'E26')
+    protected String e26;
+    @XmlElement(name = 'E27')
+    protected String e27;
+    @XmlElement(name = 'E28')
+    protected String e28;
+    @XmlElement(name = 'E29')
+    protected String e29;
+    @XmlElement(name = 'E30')
+    protected String e30;
+    @XmlElement(name = 'E31')
+    protected String e31;
+    @XmlElement(name = 'E32')
+    protected String e32;
+    @XmlElement(name = 'E33')
+    protected String e33;
+    @XmlElement(name = 'E34')
+    protected String e34;
+    @XmlElement(name = 'E35')
+    protected String e35;
+    @XmlElement(name = 'E36')
+    protected String e36;
+    @XmlElement(name = 'E37')
+    protected String e37;
+    @XmlElement(name = 'E38')
+    protected String e38;
+    @XmlElement(name = 'E39')
+    protected String e39;
+    @XmlElement(name = 'E40')
+    protected String e40;
+    @XmlElement(name = 'E41')
+    protected String e41;
+    @XmlElement(name = 'E42')
+    protected String e42;
+    @XmlElement(name = 'E43')
+    protected String e43;
+    @XmlElement(name = 'E44')
+    protected String e44;
+    @XmlElement(name = 'E45')
+    protected String e45;
+    @XmlElement(name = 'E46')
+    protected String e46;
+    @XmlElement(name = 'E47')
+    protected String e47;
+    @XmlElement(name = 'E48')
+    protected String e48;
+    @XmlElement(name = 'E49')
+    protected String e49;
+    @XmlElement(name = 'E50')
+    protected String e50;
+    @XmlElement(name = 'E51')
+    protected String e51;
+    @XmlElement(name = 'E52')
+    protected String e52;
+    @XmlElement(name = 'E53')
+    protected String e53;
+    @XmlElement(name = 'E54')
+    protected String e54;
+    @XmlElement(name = 'E55')
+    protected String e55;
+    @XmlElement(name = 'E56')
+    protected String e56;
+    @XmlElement(name = 'E57')
+    protected String e57;
+    @XmlElement(name = 'E58')
+    protected String e58;
+    @XmlElement(name = 'E59')
+    protected String e59;
+    @XmlElement(name = 'E60')
+    protected String e60;
+    @XmlElement(name = 'E61')
+    protected String e61;
+    @XmlElement(name = 'E62')
+    protected String e62;
+    @XmlElement(name = 'E63')
+    protected String e63;
+    @XmlElement(name = 'E64')
+    protected String e64;
+    @XmlElement(name = 'E65')
+    protected String e65;
+    @XmlElement(name = 'E66')
+    protected String e66;
+    @XmlElement(name = 'E67')
+    protected String e67;
+    @XmlElement(name = 'E68')
+    protected String e68;
+    @XmlElement(name = 'E69')
+    protected String e69;
+    @XmlElement(name = 'E70')
+    protected String e70;
+    @XmlElement(name = 'E71')
+    protected String e71;
+    @XmlElement(name = 'E72')
+    protected String e72;
+    @XmlElement(name = 'E73')
+    protected String e73;
+    @XmlElement(name = 'E74')
+    protected String e74;
+    @XmlElement(name = 'E75')
+    protected String e75;
+    @XmlElement(name = 'E76')
+    protected String e76;
+    @XmlElement(name = 'E77')
+    protected String e77;
+    @XmlElement(name = 'E78')
+    protected String e78;
+    @XmlElement(name = 'E79')
+    protected String e79;
+    @XmlElement(name = 'E80')
+    protected String e80;
+    @XmlElement(name = 'E81')
+    protected String e81;
+    @XmlElement(name = 'E82')
+    protected String e82;
+    @XmlElement(name = 'E83')
+    protected String e83;
+    @XmlElement(name = 'E84')
+    protected String e84;
+    @XmlElement(name = 'E85')
+    protected String e85;
+    @XmlElement(name = 'E86')
+    protected String e86;
+    @XmlElement(name = 'E87')
+    protected String e87;
+    @XmlElement(name = 'E88')
+    protected String e88;
+    @XmlElement(name = 'E89')
+    protected String e89;
+    @XmlElement(name = 'E90')
+    protected String e90;
+    @XmlElement(name = 'E91')
+    protected String e91;
+    @XmlElement(name = 'E92')
+    protected String e92;
+    @XmlElement(name = 'E93')
+    protected String e93;
+    @XmlElement(name = 'E94')
+    protected String e94;
+    @XmlElement(name = 'E95')
+    protected String e95;
+    @XmlElement(name = 'E96')
+    protected String e96;
+
+    public String getDst() {
+        return dst;
+    }
+    public void setDst(String value) {
+        this.dst = value;
+    }
+
+    public String getE1() {
+        return e1;
+    }
+    public void setE1(String value) {
+        this.e1 = value;
+    }
+
+    public String getE2() {
+        return e2;
+    }
+    public void setE2(String value) {
+        this.e2 = value;
+    }
+
+    public String getE3() {
+        return e3;
+    }
+    public void setE3(String value) {
+        this.e3 = value;
+    }
+
+    public String getE4() {
+        return e4;
+    }
+    public void setE4(String value) {
+        this.e4 = value;
+    }
+
+    public String getE5() {
+        return e5;
+    }
+    public void setE5(String value) {
+        this.e5 = value;
+    }
+
+    public String getE6() {
+        return e6;
+    }
+    public void setE6(String value) {
+        this.e6 = value;
+    }
+
+    public String getE7() {
+        return e7;
+    }
+    public void setE7(String value) {
+        this.e7 = value;
+    }
+    public String getE8() {
+        return e8;
+    }
+    public void setE8(String value) {
+        this.e8 = value;
+    }
+
+    public String getE9() {
+        return e9;
+    }
+    public void setE9(String value) {
+        this.e9 = value;
+    }
+
+
+}
